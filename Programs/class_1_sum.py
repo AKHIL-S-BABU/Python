ddb83fc38b3245dea5aa3cd0a4156f45c436e5a0
AKHIL-S-BABU <53579216+AKHIL-S-BABU@users.noreply.github.com>
2021-07-20 09:52:53 +0530

    Update and rename sum.py to class_1_sum.py

--- Programs/class_1_sum.py
+++ Programs/class_1_sum.py
@@ -1,17 +1,32 @@
+
+#Class Introduction
+#program to add,multiplicate,division using class function
+
+
 class calc:
     def __init__(self,aame,bame):
-        self.a=aame
+        self.a=aame #just to know that you dont have to use self.a=a all the time
         self.b=bame
+        
     def add(self):
         ad=self.a+self.b
         print("Sum is ",ad)
+        
     def mul(self):
         mul=self.a*self.b
         print("Multi is ",mul)
+        
     def div(self):
         div=self.a/self.b
         print("Div is ",div)
+
 sample=calc(4,6)
 sample.add()
 sample.mul()
 sample.div()
+
+
+#output:
+Sum is  10
+Multi is  24
+Div is  0.6666666666666666
